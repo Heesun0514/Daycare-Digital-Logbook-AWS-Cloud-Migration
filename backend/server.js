8e592d7c02df322ec7134ba12fd10e9fa9672c90
Huiseon Yi <heesun0514@gmail.com>
2026-07-24 09:53:50 +0900

    Update CORS Middleware

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,13 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     // Allow these headers
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // ✅ Allow credentials if needed
-    // res.header('Access-Control-Allow-Credentials', 'true');
+   
+
+     // ✅ Handle preflight OPTIONS requests immediately
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    
     next();
 });
 
