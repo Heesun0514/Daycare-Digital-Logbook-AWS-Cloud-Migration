59d5113cf73c803a2cf04eb490bd40cec7e32a57
Huiseon Yi <heesun0514@gmail.com>
2026-09-07 21:01:47 +0100

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ app.use(cors({
     origin: [
         'http://localhost:8080',
         'https://d2d7c2s58id62i.cloudfront.net',
+        'http://daycare-frontend-huiseon.s3-website-eu-west-1.amazonaws.com',
+        'https://daycare-frontend-huiseon.s3.eu-west-1.amazonaws.com', 
         'http://daycare-frontend-huiseon.s3-website-eu-west-1.amazonaws.com'
     ]
 }));
